37b29d229d3bff492f041e844ff805cddbd43733
narindersingh07 <maan.narinder07@gmail.com>
2016-06-23 14:25:25 +0530

    Employeedata
    NA

---
+++ src/source/Test.java
@@ -0,0 +1,13 @@
+package source;
+import source.Employee;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Employee e1=new Employee(1,"Narinder");
+		e1.printname();
+	}
+
+}
